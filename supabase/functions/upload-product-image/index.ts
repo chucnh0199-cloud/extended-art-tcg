d9db9140759fed6907ce99d1dd40737bc842904a
chucnh0199-cloud <chu.cnh0199@gmail.com>
2026-07-12 21:31:34 +0700

    ABC

---
+++ supabase/functions/upload-product-image/index.ts
@@ -0,0 +1,28 @@
+import { createClient } from "@supabase/supabase-js";
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers":
+    "authorization, x-client-info, apikey, content-type",
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
+};
+
+Deno.serve(async (req) => {
+
+  if (req.method === "OPTIONS") {
+    return new Response("ok", {
+      headers: corsHeaders,
+    });
+  }
+
+  return Response.json(
+    {
+      success: true,
+      message: "upload-product-image is working"
+    },
+    {
+      headers: corsHeaders,
+    }
+  );
+
+});
