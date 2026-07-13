94dbb244c8102f71ce592bf872f6c275c0e9d291
chucnh0199-cloud <chu.cnh0199@gmail.com>
2026-07-13 08:32:56 +0700

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://ltuxsflkildzuiukifzh.supabase.co";
 
-const SUPABASE_ANON_KEY = "ANON_KEY_CỦA_BẠN";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0dXhzZmxraWxkenVpdWtpZnpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2NjI5NzQsImV4cCI6MjA5OTIzODk3NH0.aDcB2hizFpuoSMSpOypv_cUm0kWIGK6YJQpaRIiYb6c";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
@@ -308,7 +308,5 @@ document.getElementById("logout-btn").onclick = async () => {
     window.location.href = "admin-login.html";
 
 };
-
-loadDashboard();
 
 loadOrders();
